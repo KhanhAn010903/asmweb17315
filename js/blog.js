3c9b3c4b328dcf0340fc3c3b47fe805c63959965
quangman <quangan010903@gmail.com>
2022-06-13 09:31:55 +0700

    update

--- js/blog.js
+++ js/blog.js
@@ -1,57 +1,56 @@
-let menu  = document.querySelector("#menu")
-document.querySelector("#button").onclick = () =>{
+const button = document.getElementById("button")
+const menu = document.getElementById("menu")
+button.addEventListener("click", function () {
   menu.classList.toggle("hidden")
-}
+})
 
-const work = [
-    {
-      id:1,
-      image: "https://res.cloudinary.com/https-asm2022-psi-vercel-app/image/upload/v1655086731/images/Rectangle_30_lcybdo.jpg",
-      name: "Designing Dashboards",
-      date: "2020",
-      short_description: "Designing Dashboards",
-      description: "   Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniamconsequat sunt nostrud amet.",
-    },
-    { 
-      id:2,
-      image: "https://res.cloudinary.com/https-asm2022-psi-vercel-app/image/upload/v1655086731/images/Rectangle_32_dwu7p5.jpg",
-      name: "Designing Dashboards",
-      date: "2020",
-      short_description: "Designing Dashboards",
-      description: "   Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniamconsequat sunt nostrud amet.",
-    },
-    { 
-      id:3,
-      image: "https://res.cloudinary.com/https-asm2022-psi-vercel-app/image/upload/v1655086731/images/Rectangle_34_guufie.jpg",
-      name: "Designing Dashboards",
-      date: "2020",
-      short_description: "Designing Dashboards",
-      description: "   Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniamconsequat sunt nostrud amet.",
-    },
-  ]
-  function showWork(work){
-      let result =""
-      for(let i=0 ; i<work.length;i++){
-          result += ` <div class="item1 border-b py-8 md:grid md:grid-cols-[246px,auto] gap-8">
-          <div><img class="w-full rounded-lg" src="${work[i].image}" alt=""></div>
-          <div class="mt-5 md:-mt-2">
-            <h3 class="text-3xl font-bold ">${work[i].name}</h3>
-            <div class="flex space-x-4 items-center py-5">
-              <span class="inline-block bg-[#142850] py-1 px-2 text-white rounded-full">${work[i].date}</span>
-              <span>${work[i].short_description}</span>
-            </div>
-            <p>
-            ${work[i].description}
-            </p>
-          </div>
-        </div>`
-      }
-      return result
+
+
+
+const listContent = [
+  {
+    id: 1,
+    title: "UI Interactions of the week",
+    date: "12 Feb 2019",
+    short_description: "Express, Handlebars",
+    description: " Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+  },
+  {
+    id: 2,
+    title: "UI Interactions of the week",
+    date: "12 Feb 2019",
+    short_description: "Express, Handlebars",
+    description: " Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+  },
+  {
+    id: 3,
+    title: "UI Interactions of the week",
+    date: "12 Feb 2019",
+    short_description: "Express, Handlebars",
+    description: " Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
   }
-  function render(elementId , content){
-      const id = document.getElementById(elementId)
-      if(id){
-          id.innerHTML = content
-      }
+];
+function showContent(listContent) {
+  let result = "";
+  for (let i = 0; i < listContent.length; i++) {
+    result += `   <div class="item py-8  border-b">
+         <h3 class="py-5"><a href="" class="text-2xl font-bold">${listContent[i].title}</a></h3>
+         <div class="flex space-x-4">
+           <span>${listContent[i].date}</span>
+           <span>|</span>
+           <span>${listContent[i].short_description}</span>
+         </div>
+      <p>
+        ${listContent[i].description}
+         </p>
+       </div>`
   }
-  render("work",showWork(work))
+  return result
+}
+function render(elementId, content) {
+  const id = document.getElementById(elementId)
+  if (id) {
+    id.innerHTML = content
+  }
+}
+render("content", showContent(listContent))
